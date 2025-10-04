6ab2bcaaa1ca27c8b80a9a1746c7135d57681f01
josecruzp-enee <j90nikol@gmail.com>
2025-10-04 12:09:04 -0600

    Update formularios.py

--- modulo/formularios.py
+++ modulo/formularios.py
@@ -6,8 +6,6 @@ Autor: José Nikol Cruz
 
 import streamlit as st
 from datetime import datetime
-from modulo.calibres import seleccionar_calibres_formulario
-
 
 def formulario_datos_proyecto():
     """Formulario para ingresar los datos generales del proyecto."""
@@ -20,13 +18,6 @@ def formulario_datos_proyecto():
     responsable = st.text_input("👷‍♂️ Responsable / Diseñador", value=st.session_state.get("responsable", ""))
     nivel_tension = st.selectbox("⚡ Nivel de Tensión (kV)", ["13.8", "34.5"], index=0)
 
-    # --- Sección 2: Calibres (sin Excel) ---
-    st.markdown("### 2️⃣ Selección de Calibres de Conductores")
-
-    calibres_seleccionados = seleccionar_calibres_formulario(
-        st.session_state.get("datos_proyecto", {})
-    )
-
     # --- Guardar datos ---
     st.session_state["datos_proyecto"] = {
         "nombre_proyecto": nombre,
@@ -34,10 +25,8 @@ def formulario_datos_proyecto():
         "empresa": empresa,
         "responsable": responsable,
         "nivel_de_tension": nivel_tension,
-        **calibres_seleccionados,
         "fecha_informe": datetime.today().strftime("%d/%m/%Y")
     }
-
 
 def mostrar_datos_formateados():
     """Muestra los datos ingresados de forma ordenada."""
