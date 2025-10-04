4e7795321f9333695f2a4fa70474191dec7cd9d1
josecruzp-enee <j90nikol@gmail.com>
2025-10-03 22:53:13 -0600

    Update generar_pdfs.py

--- modulo/generar_pdfs.py
+++ modulo/generar_pdfs.py
@@ -1,4 +1,4 @@
-# modulo/generar_pdfs.py
+# -*- coding: utf-8 -*-
 import streamlit as st
 from modulo.procesar_materiales import procesar_materiales
 from modulo.pdf_utils import (
@@ -6,13 +6,19 @@ from modulo.pdf_utils import (
     generar_pdf_estructuras_global,
     generar_pdf_estructuras_por_punto,
     generar_pdf_materiales_por_punto,
-    generar_pdf_completo,
+    generar_pdf_completo
 )
 
-
-COLUMNAS_BASE = ["Punto", "Poste", "Primario", "Secundario", "Retenida", "Aterrizaje", "Transformador"]
+COLUMNAS_BASE = [
+    "Punto", "Poste", "Primario", "Secundario",
+    "Retenida", "Aterrizaje", "Transformador"
+]
 
 def generar_pdfs(modo_carga, archivo_estructuras, df, ruta_datos_materiales):
+    """
+    Genera los diferentes reportes PDF a partir de los datos del proyecto.
+    Retorna un diccionario con los buffers PDF.
+    """
     # Procesar datos base
     df_resumen, df_estructuras_resumen, df_resumen_por_punto, datos_proyecto = procesar_materiales(
         archivo_estructuras=archivo_estructuras,
@@ -29,7 +35,12 @@ def generar_pdfs(modo_carga, archivo_estructuras, df, ruta_datos_materiales):
         "estructuras_global": generar_pdf_estructuras_global(df_estructuras_resumen, nombre_proyecto),
         "estructuras_por_punto": generar_pdf_estructuras_por_punto(df_resumen_por_punto, nombre_proyecto),
         "materiales_por_punto": generar_pdf_materiales_por_punto(df_resumen_por_punto, nombre_proyecto),
-        "completo": generar_pdf_completo(df_resumen, df_estructuras_resumen, df_resumen_por_punto, datos_proyecto),
+        "completo": generar_pdf_completo(
+            df_resumen,
+            df_estructuras_resumen,
+            df_resumen_por_punto,
+            datos_proyecto
+        ),
     }
 
     return pdfs
