8988d48070e152421c4d45ae52f163972d4f04b8
josecruzp-enee <j90nikol@gmail.com>
2025-10-23 21:26:26 -0600

    Update datos_proyecto.py

--- interfaz/datos_proyecto.py
+++ interfaz/datos_proyecto.py
@@ -1,1 +1,10 @@
+# -*- coding: utf-8 -*-
+# interfaz/datos_proyecto.py
 
+import streamlit as st
+from modulo.formularios import formulario_datos_proyecto, mostrar_datos_formateados
+
+def seccion_datos_proyecto() -> None:
+    """Muestra formulario y resumen de datos generales del proyecto."""
+    formulario_datos_proyecto()
+    mostrar_datos_formateados()
