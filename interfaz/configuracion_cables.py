8df0c777666106d973b96a981af3519fdde2c84e
josecruzp-enee <j90nikol@gmail.com>
2026-01-30 15:46:18 -0600

    Update configuracion_cables.py

--- interfaz/configuracion_cables.py
+++ interfaz/configuracion_cables.py
@@ -5,7 +5,8 @@ Fachada (compatibilidad): reexporta UI y PDF.
 """
 
 from .cables_ui import seccion_cables
-from .cables_pdf import tabla_cables_pdf
+from exportadores.cables_pdf import tabla_cables_pdf
+
 
 # Si en otros lados importabas helpers, los podés reexportar:
 from .cables_catalogo import get_tipos, get_calibres, get_calibres_union, get_configs_por_tipo, get_configs_union
