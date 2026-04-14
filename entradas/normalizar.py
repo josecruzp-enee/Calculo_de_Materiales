9a542cb3a96ca2349309ab8780eddde9060c30fb
josecruzp-enee <j90nikol@gmail.com>
2026-04-14 14:17:06 -0600

    Update normalizar.py

--- entradas/normalizar.py
+++ entradas/normalizar.py
@@ -54,7 +54,8 @@ PATRON = re.compile(
     r"""
     \b(A-[IVX]+-\d+[A-Z]?)\b|
     \b(B-[IVX]+-\d+[A-Z]?)\b|
-    \b(C[S]-\d+)\b|
+    \b(CS-\d+)\b|      
+    \b(CA-\d+)\b|  
     \b(P[CMT][A-Z]?-\d+)\b|
     \b(TS-\d+(?:\.\d+)?KVA)\b|
     \b(CT-[A-Z])\b|
