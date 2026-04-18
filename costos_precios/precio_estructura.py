6cc7c49fd3fa86701c9f6475517f2ed36cb906d5
josecruzp-enee <j90nikol@gmail.com>
2026-04-18 13:04:07 -0600

    Update precio_estructura.py

--- costos_precios/precio_estructura.py
+++ costos_precios/precio_estructura.py
@@ -254,9 +254,9 @@ def _agregar_cable_a_precios(df_precios, entrada):
         # FILA
         # =====================================================
         filas.append({
-            "Estructura": f"LP {calibre} | {int(longitud_tramo)}m | {fases}F"
-                  if tipo.startswith("MT")
-                  else f"LS-{calibre} ({int(longitud_tramo)} m en {fases}F)"
+            "Estructura": (f"LÍNEA MT {calibre} | {int(longitud_tramo)} m | {fases}F"
+                if tipo.startswith("MT")
+                else f"LÍNEA BT {calibre} | {int(longitud_tramo)} m | {fases}F"),
             "Cantidad": longitud,
             "Costo Unitario": precio,
             "Costo Operativo": 0,
