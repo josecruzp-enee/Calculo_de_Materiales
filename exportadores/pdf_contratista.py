cb97f4832b6d7780081735ec4d8a31845a2b5f84
josecruzp-enee <j90nikol@gmail.com>
2026-04-29 10:21:55 -0600

    Update pdf_contratista.py

--- exportadores/pdf_contratista.py
+++ exportadores/pdf_contratista.py
@@ -90,12 +90,12 @@ def tabla_presupuesto_general(df_detalle):
         total += r["Subtotal"]
 
     # COSTOS
-    horas_grua = 147
+    horas_grua = 84
     precio_hora = 1700
     total_grua = horas_grua * precio_hora
 
     flete_unit = 25000
-    viajes = 4
+    viajes = 2
     flete_total = flete_unit * viajes
 
     ingenieria = 25000
