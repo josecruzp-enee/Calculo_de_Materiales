fb14a6209e484e99383371aba8c4a0a179007f96
josecruzp-enee <j90nikol@gmail.com>
2026-07-08 09:12:03 -0600

    Update normalizar.py

--- entradas/normalizar.py
+++ entradas/normalizar.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 import re
 import pandas as pd
-
+import streamlit as st
 
 # =========================================================
 # LIMPIEZA DXF (CRÍTICO)
